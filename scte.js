d8fcdbd43e14ea8d9ab1bef252c771e729a67096
prakriti5dhang <41319546+prakriti5dhang@users.noreply.github.com>
2023-08-10 02:38:10 -0700

    files updated

--- scte.js
+++ scte.js
@@ -1,76 +1,96 @@
-var ctx1 = null;
-var canvas1;
-var pointSize=2;
-var arr= [];
-var quotient1,quotient2;
-var tabrowindex=0;
+var ctx1;
+var gcanvas1;
+var pointSize = 2;
+var arr = [];
+var quotient1, quotient2;
+var tabrowindex = 0;
 var xyz = new Array();
 var cell;
 var table;
-var counter=0;
-var x,y;
-var count1=0;
+var counter = 0;
+var x, y;
+var count1 = 0;
 var coordinates1 = new Array();
 var coordinates2 = new Array();
 var classes = new Array();
 var xCo = new Array();
 var yCo = new Array();
-var transX2,transY2;
-var temp1,temp2;
-var i=0;
-var tabrowindex=0;    
+var transX, transY;
+var temp1, temp2;
+var i = 0;
+var tabrowindex = 0;
+var points = []; //points to draw stoke
+var dataPointsg=[]; // for chart
+var chart;
 
-
-$(document).ready(function(){
-    $("#mycanvas1").click(function(e){
+$(document).ready(function () {
+    $("#mycanvas1").click(function (e) {
         getPosition1(e);
     });
 });
 
 function canvas11() {
-canvas1 = document.getElementById("mycanvas1"),
-ctx1 = canvas1.getContext('2d'),
-transX2 = canvas1.width*0 , //21
-transY2 = canvas1.height * 1; //399
-ctx1.translate(transX2, transY2);
-ctx1.fillRect(0,-transY2, 1, canvas1.height); //vertical Axis
-ctx1.fillRect( - transX2, 0, canvas1.width, 1); //Horizantal Axis
-canvas1.onmousemove = function(e) {
-var pos1 = getMousePos1(canvas1, e);
-//out.innerHTML = 'X:' + pos1.x + ' Y:' + pos1.y;
-}
+    gcanvas1 = document.getElementById("mycanvas1"),
+        ctx1 = gcanvas1.getContext('2d'),
+        transX = gcanvas1.width * 0, //21
+        transY = gcanvas1.height * 1; //399
+    ctx1.translate(transX, transY);
+    ctx1.fillRect(0, -transY, 1, gcanvas1.height); //vertical Axis
+    ctx1.fillRect(- transX, 0, gcanvas1.width, 1); //Horizantal Axis
+    gcanvas1.onmousemove = function (e) {
+        var pos1 = getMousePos1(gcanvas1, e);
+        //out.innerHTML = 'X:' + pos1.x + ' Y:' + pos1.y;
+    }
+
+    chart = new CanvasJS.Chart("ChartContainer", {
+        animationEnabled: true,
+            title: {
+                text: "RBF Prediction",
+            },
+           
+            data: [{
+                    type: "spline",
+                    dataPoints: dataPointsg
+                }]
+        });
+    
+        chart.render();
+    
+       
+       
 };
 var coordinates = new Array();
-function getPosition1(event){
-var rect = canvas1.getBoundingClientRect();
-x = event.clientX - rect.left- transX2;
-y = event.clientY - rect.top- transY2;
-drawCoordinates(x,y);
-tabled();
+function getPosition1(event) {
+    var rect = gcanvas1.getBoundingClientRect();
+    x = event.clientX - rect.left - transX;
+    y = event.clientY - rect.top - transY;
+    drawCoordinates(x, y);
+    tabled();
 }
 
 
-function getMousePos1(canvas1, evt) {
-var rect = canvas1.getBoundingClientRect();
-return {
-x: evt.clientX - (rect.left + 0.5) - transX2,
-y: evt.clientY - rect.top - transY2
-       };
+function getMousePos1(gcanvas1, evt) {
+    var rect = gcanvas1.getBoundingClientRect();
+    return {
+        x: evt.clientX - (rect.left + 0.5) - transX,
+        y: evt.clientY - rect.top - transY
+    };
 }
 
-function drawCoordinates(x,y){	
-xCo.push(x);yCo.push(y);
-if(counter < 100) {
-var ctx1 = document.getElementById("mycanvas1").getContext("2d");
-ctx1.beginPath();
-ctx1.arc(x, y, pointSize, 0, Math.PI * 2, true);
-ctx1.lineWidth=1;
-ctx1.strokeStyle="blue";
-ctx1.fillStyle="blue";
-ctx1.fill();
-ctx1.stroke();
+function drawCoordinates(x, y) {
+    xCo.push(x); yCo.push(y);
+    points.push({ x, y });
+    if (counter < 100) {
+        ctx1 = document.getElementById("mycanvas1").getContext("2d");
+        ctx1.beginPath();
+        ctx1.arc(x, y, pointSize, 0, Math.PI * 2, true);
+        ctx1.lineWidth = 1;
+        ctx1.strokeStyle = "blue";
+        ctx1.fillStyle = "blue";
+        ctx1.fill();
+        ctx1.stroke();
 
-    if ((x>= 0 && x <= 225) && (y >= -400 && y <= -200)){
+        /* f ((x>= 0 && x <= 225) && (y >= -400 && y <= -200)){
         classes.push(1);
     } else if ((x >= 225 && x <= 450) && (y >= -400 && y <= -200)) {
         classes.push(2);
@@ -80,174 +100,273 @@ ctx1.stroke();
         //alert(isPoint1 +"   "+isPoint2);
         if(is_in_triangle(x,y,225,-200,220,0,450,0)) classes.push(3);
         if(is_in_triangle(x,y,225,-200,450,-200,450,0)) classes.push(2);
-    }
+    } */
     }
     //alert(classes);
-temp1 = Math.abs(x);
-temp2 = Math.abs(y);
-quotient1 = parseFloat(temp1/ 49).toPrecision(6); // 11
-quotient2 = parseFloat(temp2 / 52).toPrecision(6); // 3
-var cord = quotient1+"|"+quotient2;
-xyz.push(cord);
-//alert("cord");
-coordinates1[count1] = quotient1;
-coordinates2[count1] = quotient2;
-count1++;
+    temp1 = Math.abs(x);
+    temp2 = Math.abs(y);
+    quotient1 = parseFloat(temp1 / 49).toPrecision(6); // 11
+    quotient2 = parseFloat(temp2 / 52).toPrecision(6); // 3
+    var cord = quotient1 + "|" + quotient2;
+    xyz.push(cord);
+    //alert("cord");
+    coordinates1[count1] = quotient1;
+    coordinates2[count1] = quotient2;
+    count1++;
 }
 
 
 
 function tabled() {
-table = document.getElementById("mytable");
+    table = document.getElementById("mytable");
 
-arr[0] = tabrowindex + 1;
-arr[1] = quotient1;//x
-arr[2] = quotient2;//y
+    arr[0] = tabrowindex + 1;
+    arr[1] = quotient1;//x
+    arr[2] = quotient2;//y
 
 
-if (table.rows.length <= 100) 
-{
-    var row = table.insertRow(++tabrowindex); // Row increment
-    for (var q = 0; q < 3; q++) 
-    {
-        cell = row.insertCell(q);
-        cell.innerHTML = arr[q];
-        document.getElementById("tpdata").innerHTML="Added training point " + arr[0]+" at ( "+arr[1]+" , "+arr[2]+" )";
+    if (table.rows.length <= 100) {
+        var row = table.insertRow(++tabrowindex); // Row increment
+        for (var q = 0; q < 3; q++) {
+            cell = row.insertCell(q);
+            cell.innerHTML = arr[q];
+            document.getElementById("tpdata").innerHTML = "Added training point " + arr[0] + " at ( " + arr[1] + " , " + arr[2] + " )";
+        }
     }
 }
+
+
+function reset() {
+    ctx1.fillStyle = "black";
+    ctx1.clearRect(0, -200, gcanvas1.width, gcanvas1.height);
+    //ctx1.clearRect(0, 0, canvas1.width, canvas1.height);
+    ctx1.fillRect(0, -transY, 1, gcanvas1.height); //vertical Axis
+    ctx1.fillRect(- transX, 0, gcanvas1.width, 1); //Horizantal Axis
+    var rowCount = table.rows.length;
+    for (var j = rowCount - 1; j >= 1; j--) {
+        table.deleteRow(j);
+        points.pop();
+        dataPointsg.pop();
+    }
+    tabrowindex = 0;
+    i = 0;
+    document.getElementById("tpdata").innerHTML = "";
+    //console.log(xyz);
+    xyz = [];
+    classes = [];
+    clearChart();
 }
 
-
-function reset(){
-ctx1.fillStyle = "black";
-ctx1.clearRect( 0, -400, canvas1.width, canvas1.height);
-ctx1.fillRect(0, - transY2, 1, canvas1.height); //vertical Axis
-ctx1.fillRect( - transX2, 0, canvas1.width, 1); //Horizantal Axis
-var rowCount = table.rows.length;
-for (var j = rowCount -1; j >= 0; j--) {
-table.deleteRow(j);
-   }
-tabrowindex = 0;
-i=0;
- //console.log(xyz);
-xyz=[];
-classes=[];
+/************************ Function for draw strokes ***************************/
+function drawStroke(ctx, x1, y1, x2, y2) {
+    ctx.beginPath();
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.stroke();
 }
-
-
-
-function classify(){
-rect1();
-rect2();
-rect3();
-tri1();
-tri2();
-//alert(classes.length);
-for(i=0; i<classes.length; i++) {
-var tree=table.rows[i].cells;
-tree[3].innerHTML = classes[i];
-}
-moveRight();
-}
-
-
-//var animate, left=0, imgObj=null, report = document.getElementById('report'), i=0;
-function moveRight(){
-/*imgObj.style.setProperty(" -webkit-transition", " width 2s");
-imgObj.style.setProperty("transition", "width 2s"); }*/
-for ( tabrowindex = 0; tabrowindex < table.rows.length; tabrowindex++) {
-var rwr1 = table.rows[tabrowindex].cells;
-ctx1.beginPath();
-ctx1.lineWidth=1;
-ctx1.arc(((parseFloat(rwr1[1].innerHTML))*49), (-(parseFloat(rwr1[2].innerHTML))*52), pointSize, 0, Math.PI * 2, true);
-ctx1.strokeStyle = 'black';
-ctx1.stroke();
- }
-}
-
-function rect1()
-{
-    ctx1.beginPath();
-    ctx1.moveTo(0,-400);
-    ctx1.lineTo(225,-400);
-    ctx1.lineTo(225, -200);
-    ctx1.lineTo(0, -200);
-    ctx1.closePath();
-    ctx1.lineWidth = 2;
-    ctx1.stroke();
-    ctx1.fillStyle='#0040ff';
-    ctx1.fill();
-}
-
-function rect2()
-{
-    ctx1.beginPath();
-    ctx1.moveTo(225,-400);
-    ctx1.lineTo(450,-400);
-    ctx1.lineTo(450, -200);
-    ctx1.lineTo(225, -200);
-    ctx1.closePath();
-    ctx1.lineWidth = 2;
-    //ctx1.strokeStyle = 'black';
-    ctx1.stroke();
-    ctx1.fillStyle='red';
-    ctx1.fill();
-}
-function rect3()
-{
-    ctx1.beginPath();
-    ctx1.moveTo(0,-200);
-    ctx1.lineTo(225,-200);
-    ctx1.lineTo(225, 0);
-    ctx1.lineTo(0, 0);
-    ctx1.closePath();
-    ctx1.lineWidth = 2;
-    ctx1.stroke();
-    ctx1.fillStyle='#66ff33';
-    ctx1.fill();
-    
-}
-function tri1()
-{
-    ctx1.beginPath();
-    ctx1.moveTo(225,-200);
-    ctx1.lineTo(225, 0);
-    ctx1.lineTo(450, 0);
-    ctx1.closePath();
-    ctx1.fillStyle='#66ff33';
-    ctx1.fill();
-}
-function tri2()
-{
-    ctx1.beginPath();
-    ctx1.moveTo(225,-200);
-    ctx1.lineTo(450,-200);
-    ctx1.lineTo(450, 0);
-    ctx1.closePath();
-    ctx1.fillStyle='red';
-    ctx1.fill();
-}
-
-
-
-function is_in_triangle (x,y,x1,y1,x2,y2,x3,y3){
-
-    var v0 = [x3-x1,y3-y1];
-    var v1 = [x2-x1,y2-y1];
-    var v2 = [x-x1,y-y1];
-    
-    var dot00 = (v0[0]*v0[0]) + (v0[1]*v0[1]);
-    var dot01 = (v0[0]*v1[0]) + (v0[1]*v1[1]);
-    var dot02 = (v0[0]*v2[0]) + (v0[1]*v2[1]);
-    var dot11 = (v1[0]*v1[0]) + (v1[1]*v1[1]);
-    var dot12 = (v1[0]*v2[0]) + (v1[1]*v2[1]);
-    
-    var invDenom = 1/ (dot00 * dot11 - dot01 * dot01);
-    
-    var u = (dot11 * dot02 - dot01 * dot12) * invDenom;
-    var v = (dot00 * dot12 - dot01 * dot02) * invDenom;
-    
-    if((u >= 0) && (v >= 0) && (u + v < 1)) {	
-        return true;
+/**************************** redraw button **********************************/
+function redraw() {
+    // Draw strokes between points
+    ctx1.strokeStyle = 'red';
+    for (let i = 0; i < points.length - 1; i++) {
+        drawStroke(ctx1, points[i].x, points[i].y, points[i + 1].x, points[i + 1].y);
     }
 }
+
+function go(){
+var centers=document.getElementById("centernum").value;
+// Gaussian Radial Basis Function
+function radialBasisFunction(input, center, width) {
+    var distance = Math.abs(input - center);
+    return Math.exp(-0.5 * (distance / width) ** 2);
+  }
+  
+  // RBF Network Prediction
+  function rbfNetworkPrediction(input, centers, weights, widths) {
+    if (centers.length !== weights.length || centers.length !== widths.length) {
+      throw new Error("The number of centers, weights, and widths should be the same.");
+    }
+  
+    let output = 0;
+    for (let i = 0; i < centers.length; i++) {
+      var rbfValue = radialBasisFunction(input, centers[i], widths[i]);
+      output += rbfValue * weights[i];
+    }
+  
+    return output;
+  }
+  
+  // Function to read data from the table and prepare arrays
+  function getDataFromTable(mytable) {
+    var table = document.getElementById(mytable);
+    var rows = table.getElementsByTagName("tr");
+  
+    // Initialize arrays to hold data
+    var inputs = [];
+    var outputs = [];
+  
+    // Skip the first row (header)
+    for (let i = 1; i < rows.length; i++) {
+      var cells = rows[i].getElementsByTagName("td");
+      var input = parseFloat(cells[1].innerText);
+      var output = parseFloat(cells[2].innerText);
+  
+      inputs.push(input);
+      outputs.push(output);
+    }
+  
+    return { inputs, outputs };
+  }
+  
+  // Example usage:
+  var mytable = "mytable";
+  var { inputs, outputs } = getDataFromTable(mytable);
+  
+  // Assuming you have the centers, weights, and widths arrays from your trained RBF network
+  var centers = [ 2.0]; // Example centers array
+  var weights = [ 0.5]; // Example weights array
+  var widths = [ 1 ]; // Example widths array
+  
+  // Calculate predictions for each input in the table
+  for (let i = 0; i < inputs.length; i++) {
+    var input = inputs[i];
+    var prediction = rbfNetworkPrediction(input, centers, weights, widths);
+    console.log("Prediction for input " +input +" , " + prediction);
+    dataPointsg.push({x:input, y:prediction});
+  }
+  
+   
+  showgraph();
+
+
+}
+
+
+function showgraph(){
+    chart = new CanvasJS.Chart("ChartContainer", {
+        animationEnabled: true,
+            title: {
+                text: "RBF Prediction",
+            },
+            axisX:{
+                gridColor: "lightblue" ,
+                gridThickness: 1       
+              },
+            data: [{
+                    type: "spline",
+                    dataPoints: dataPointsg
+                }]
+        });
+    
+        chart.render();
+    
+       
+        console.log(dataPointsg);
+    }
+    
+    
+    function clearChart() {
+        chart.options.data.forEach((series) => {
+            series.dataPoints = [];
+          });
+        chart.render();
+      }
+    
+
+
+
+
+//var dataPoints=[];
+/*
+function euclideanDistance(x1, x2) {
+    return Math.abs(x1 - x2);
+}
+function radialBasisFunction(x, center, gamma) {
+    const distance = euclideanDistance(x, center);
+    return Math.sin(gamma * distance);
+}
+function calculateRBFOutputs(input, centers, gamma) {
+    return centers.map((center) => radialBasisFunction(input, center, gamma));
+}
+function calculateWeights(inputs, outputs, centers, gamma) {
+    const phiMatrix = inputs.map((input) => calculateRBFOutputs(input, centers, gamma));
+    //alert(phiMatrix);
+    const phiTranspose = math.transpose(phiMatrix);
+    const weights = math.multiply(math.multiply(math.inv(math.multiply(phiTranspose, phiMatrix)), phiTranspose), outputs);
+    return weights;
+}
+
+var dataPointsg = [];
+function plotRBFGraph(points, centers, gamma, weights) {
+    const inputs = points.map((point) => point.x);
+    const rbfOutputs = inputs.map((input) => calculateRBFOutputs(input, centers, gamma));
+    const predictions = math.multiply(rbfOutputs, weights) ;
+
+    dataPointsg = inputs.map((input, index) => ({
+        x: input ,
+        y: predictions[index] *(-1) ,
+    }));
+}
+
+
+
+
+
+
+
+function calculate() {
+    const gamma = 1;
+    const centers = points.map((point) => point.x);
+    const weights = calculateWeights(points.map((point) => point.x), points.map((point) => point.y), centers, gamma);
+    plotRBFGraph(points, centers, gamma, weights);
+
+
+}
+
+
+
+
+
+
+function go() {
+    calculate();
+
+    showgraph();
+
+
+    
+
+}
+
+
+
+
+
+
+ function showgraph(){
+    chart = new CanvasJS.Chart("ChartContainer", {
+        animationEnabled: true,
+            title: {
+                text: "RBF Prediction",
+            },
+            axisX:{
+                gridColor: "lightblue" ,
+                gridThickness: 1       
+              },
+            data: [{
+                    type: "spline",
+                    dataPoints: dataPointsg
+                }]
+        });
+    
+        chart.render();
+    
+       
+        console.log(dataPointsg);
+    } 
+    
+
+
+*/
+
+
